c0deb10b772250c9b664483e79a2350ddef206f9
Shiv Kumar Khanna <shikhanna@altimetrik.com>
2025-05-09 13:19:44 +0530

    test

--- Project-2/vars/testSharedLibrary.groovy
+++ Project-2/vars/testSharedLibrary.groovy
@@ -1,14 +1,13 @@
-def call() 
-  {
+def call() {
             echo 'Testing shared libraries in jenkins.'
             sh '''
-                echo " What is my present working directory."
-                pwd
-                echo "List all the files and folders in present working directory."
-                ls -al
-                echo "Checking disk space alloted to OS."
-                df -h
-                echo " Checking avaiable menory on the system"
-                free -m
-            '''
+                  echo " What is my present working directory."
+                  pwd
+                  echo "List all the files and folders in present working directory."
+                  ls -al
+                  echo "Checking disk space alloted to OS."
+                  df -h
+                  echo " Checking avaiable menory on the system"
+                  free -m
+             '''
   }
