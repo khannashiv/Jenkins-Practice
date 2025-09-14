eac2f0e5eaa16fd5f6165f04fc25b5a0a39f5691
Shiv <khannashiv94@gmail.com>
2025-09-14 05:53:51 +0000

    test

--- CJE/jenkins_setup_interface/jenkins_freestyle_project_demo.sh
+++ CJE/jenkins_setup_interface/jenkins_freestyle_project_demo.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-# Build Stage.
+#### Build Stage.
 
 # Fix PATH first (for Jenkins, because Jenkins PATH doesn’t include /usr/games)
 export PATH=$PATH:/usr/games:/usr/local/games
@@ -11,7 +11,7 @@ curl -s https://api.adviceslip.com/advice > advice.json
 # Extract advice text
 jq -r .slip.advice advice.json > advice.message
 
-# Test to make sure the advice message has more than 5 words ( Test Stage. )
+#### Test to make sure the advice message has more than 5 words ( Test Stage. )
 word_count=$(wc -w < advice.message)
 
 if [ $word_count -gt 5 ]; then
@@ -20,7 +20,7 @@ else
   echo "Advice $(cat advice.message) has 5 words or less"
 fi
 
-# Ensure cowsay is installed. ( Deploy Stage. )
+#### Ensure cowsay is installed. ( Deploy Stage. )
 if [ ! command -v cowsay &> /dev/null ]; then
   echo "Error: cowsay is not installed. Please run: sudo apt-get install cowsay -y"
   exit 1
